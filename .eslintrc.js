5f2814a3a2318eeaa1f9e2a35c8675427bb8e769
Faiz Kasman <faiz-kasman@parkeasy.co>
2025-02-28 03:53:27 +0800

    feat: Add project configuration files
    - Add package.json with dependencies\n- Add TypeScript configuration\n- Add ESLint configuration\n- Add Prettier configuration\n- Add Jest configuration

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    jest: true,
+    es2020: true
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+  },
+  settings: {
+    jest: {
+      version: 'detect'
+    }
+  }
+}; 
